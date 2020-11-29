80c968279700caf9b38d8a54fcb230d6477032c4
ekokurniawan <ekokurniawankw96@gmail.com>
2020-11-30 03:58:33 +0700

    Revert "tugas minggu 6&7"
    This reverts commit c5936c600d6bf8c8786f4c4180829287ab353bfb.

--- index.php
+++ index.php
@@ -15,9 +15,9 @@ define('LARAVEL_START', microtime(true));
 | instead of starting the framework, which could cause an exception.
 |
 */
-
-if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
-    require __DIR__.'/../storage/framework/maintenance.php';
+$system_dir = __DIR__ . "/system";
+if (file_exists($system_dir . '/storage/framework/maintenance.php')) {
+    require $system_dir . '/storage/framework/maintenance.php';
 }
 
 /*
@@ -31,7 +31,7 @@ if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require $system_dir . '/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +44,10 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once $system_dir . '/bootstrap/app.php';
+$app->bind('path.public', function () {
+    return __DIR__ . "/public";
+});
 
 $kernel = $app->make(Kernel::class);
 
